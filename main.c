df9bfac03e6e381eb746ba7915d16622107cbdd1
12999311 <12999311@itisrossi.vi.it>
2025-10-08 11:29:56 +0200

    esercizio in classe

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
-}
+    char frase[] = "i topi non avevano nipoti";
+    char *end = (frase + sizeof(frase) - 1); //-1!!!
+    //avrei potuto fare:
+    //while(*end != '\0'){
+    // end++;
+    char *start = frase;
+
+    while (start <= end){
+        if (*start)
